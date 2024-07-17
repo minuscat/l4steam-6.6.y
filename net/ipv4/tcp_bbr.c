198021a456ae8d9e6296c3693232d8e5f00748e8
Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
2024-07-17 20:11:30 +0200

    tcp: Replace min_tso_segs() with tso_segs() CC callback
    This patch replaces existing min_tso_segs() with tso_segs() CC callbak
    for CC algorithm to provides explicit tso segment number of each data
    burst and overrides tcp_tso_autosize().
    
    This change provides below impacts on BPF struct_ops users:
    - The callback is renamed from min_tso_segs to tso_segs
    - The signature gains an extra u32 mss_now argument
    - The return value semantics is changed from "floor value passed into
      tcp_tso_autosize()" to "final tso_segs value", bypassing autosizing
    
    As a result, BPF programs shall be updated, beccause retuning a small
    constans will now directly limit tso_segs instead of the minimum.
    
    Signed-off-by: Ilpo Järvinen <ij@kernel.org>
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>

--- net/ipv4/tcp_bbr.c
+++ net/ipv4/tcp_bbr.c
@@ -295,9 +295,16 @@ static void bbr_set_pacing_rate(struct sock *sk, u32 bw, int gain)
 }
 
 /* override sysctl_tcp_min_tso_segs */
-__bpf_kfunc static u32 bbr_min_tso_segs(struct sock *sk)
+static u32 bbr_min_tso_segs(struct sock *sk)
 {
 	return sk->sk_pacing_rate < (bbr_min_tso_rate >> 3) ? 1 : 2;
+}
+
+__bpf_kfunc static u32 bbr_tso_segs(struct sock *sk, u32 mss_now)
+{
+	if (unlikely(!mss_now))
+		return U32_MAX;
+	return tcp_tso_autosize(sk, mss_now, bbr_min_tso_segs(sk));
 }
 
 static u32 bbr_tso_segs_goal(struct sock *sk)
@@ -1149,7 +1156,7 @@ static struct tcp_congestion_ops tcp_bbr_cong_ops __read_mostly = {
 	.undo_cwnd	= bbr_undo_cwnd,
 	.cwnd_event	= bbr_cwnd_event,
 	.ssthresh	= bbr_ssthresh,
-	.min_tso_segs	= bbr_min_tso_segs,
+	.tso_segs	= bbr_tso_segs,
 	.get_info	= bbr_get_info,
 	.set_state	= bbr_set_state,
 };
@@ -1163,7 +1170,7 @@ BTF_ID_FLAGS(func, bbr_sndbuf_expand)
 BTF_ID_FLAGS(func, bbr_undo_cwnd)
 BTF_ID_FLAGS(func, bbr_cwnd_event)
 BTF_ID_FLAGS(func, bbr_ssthresh)
-BTF_ID_FLAGS(func, bbr_min_tso_segs)
+BTF_ID_FLAGS(func, bbr_tso_segs)
 BTF_ID_FLAGS(func, bbr_set_state)
 #endif
 #endif
